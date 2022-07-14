9a0942bafd5e94f81bda1b6c2e58c53ef8fa256f
B Haney <43055154+Bhaney44@users.noreply.github.com>
2022-07-13 19:08:05 -0700

    Update constants.js

--- ComplianceWebApp/src/constants.js
+++ ComplianceWebApp/src/constants.js
@@ -1,4 +1,4 @@
 //Replace the "constants.js" with this in order to run on MainNet
 const ASSET_ID =  42771692;
-const ComplianceAddress = 'BSW4FRTCT2SXKVK6P53I57SEAOCCPD6TYAS77YUU725KCY6U7EM2LLJOEI'
+const ComplianceAddress = ''
 export { ASSET_ID, ComplianceAddress};
